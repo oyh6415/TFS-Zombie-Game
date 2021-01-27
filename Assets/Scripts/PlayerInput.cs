fc2ed40bcc0c897e24f5e7553a7b199c4a78a0dc
oyh6415 <dudgus6324@naver.com>
2021-01-27 23:12:40 +0900

    플레이어 움직임 구현
    * 키보드 입력 스크립트
    * 캐릭터 이동 스크립트
     - 앞뒤양옆
     - 점프
     - 카메라를 따라 캐릭터 로테이션 변경

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -2,22 +2,23 @@
 
 public class PlayerInput : MonoBehaviour
 {
-    public string fireButtonName = "Fire1";
-    public string jumpButtonName = "Jump";
-    public string moveHorizontalAxisName = "Horizontal";
-    public string moveVerticalAxisName = "Vertical";
-    public string reloadButtonName = "Reload";
+    public string fireButtonName = "Fire1"; //총 발사
+    public string jumpButtonName = "Jump"; //점프
+    public string moveHorizontalAxisName = "Horizontal"; //좌우
+    public string moveVerticalAxisName = "Vertical"; //앞뒤
+    public string reloadButtonName = "Reload"; //재장전 (커스텀)
 
-    public Vector2 moveInput { get; private set; }
+
+    public Vector2 moveInput { get; private set; } //private set -> 외부에서 수정할 수 없음
     public bool fire { get; private set; }
     public bool reload { get; private set; }
     public bool jump { get; private set; }
     
     private void Update()
     {
-        if (GameManager.Instance != null
-            && GameManager.Instance.isGameover)
-        {
+        if (GameManager.Instance != null //게임매니저가 존재하고
+            && GameManager.Instance.isGameover) //게임오버가 됐다면
+        { //유저의 입력 모두 무시
             moveInput = Vector2.zero;
             fire = false;
             reload = false;
@@ -26,7 +27,8 @@ public class PlayerInput : MonoBehaviour
         }
 
         moveInput = new Vector2(Input.GetAxis(moveHorizontalAxisName), Input.GetAxis(moveVerticalAxisName));
-        if (moveInput.sqrMagnitude > 1) moveInput = moveInput.normalized;
+        if (moveInput.sqrMagnitude > 1) moveInput = moveInput.normalized; //대각선 값이 1을 넘으면 정규화, 1로 만듬
+        // 대각선 속도가 빨라지지 않기 위함
 
         jump = Input.GetButtonDown(jumpButtonName);
         fire = Input.GetButton(fireButtonName);
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,9 +8,9 @@ public class PlayerMovement : MonoBehaviour
     
     private Camera followCam;
     
-    public float speed = 6f;
-    public float jumpVelocity = 20f;
-    [Range(0.01f, 1f)] public float airControlPercent;
+    public float speed = 6f; //움직임 속도
+    public float jumpVelocity = 20f; //점프 속도
+    [Range(0.01f, 1f)] public float airControlPercent; //공중에 체류하는 동안 몇 퍼센트 확률로 통제하는지
 
     public float speedSmoothTime = 0.1f;
     public float turnSmoothTime = 0.1f;
@@ -18,19 +18,22 @@ public class PlayerMovement : MonoBehaviour
     private float speedSmoothVelocity;
     private float turnSmoothVelocity;
     
-    private float currentVelocityY;
+    private float currentVelocityY; //중력이 없기 때문에
     
     public float currentSpeed =>
-        new Vector2(characterController.velocity.x, characterController.velocity.z).magnitude;
+        new Vector2(characterController.velocity.x, characterController.velocity.z).magnitude; //벡터의 길기(대각선)
     
     private void Start()
     {
-
+        playerInput = GetComponent<PlayerInput>();
+        animator = GetComponent<Animator>();
+        characterController = GetComponent<CharacterController>();
+        followCam = Camera.main;
     }
 
     private void FixedUpdate()
     {
-        if (currentSpeed > 0.2f || playerInput.fire) Rotate();
+        if (currentSpeed > 0.2f || playerInput.fire) Rotate(); //플레이어가 속도를 내거나 총을 쏘면 카메라 회전
 
         Move(playerInput.moveInput);
         
@@ -39,26 +42,49 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        
+        UpdateAnimation(playerInput.moveInput);
     }
 
     public void Move(Vector2 moveInput)
     {
+        var targetSpeed = speed * moveInput.magnitude;
+        var moveDirection =Vector3.Normalize
+            (transform.forward * moveInput.y + transform.right * moveInput.x);
 
+        var smoothTime = characterController.isGrounded ? speedSmoothTime : speedSmoothTime / airControlPercent;
+        // 땅에 닿지 않았다면 느리게 조작
+
+        targetSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedSmoothVelocity, smoothTime); //지연
+
+        currentVelocityY += Time.deltaTime * Physics.gravity.y; //중력가속도만큼 y축에 더함
+
+        var velocity = moveDirection * targetSpeed + Vector3.up * currentVelocityY; //최종값
+
+        characterController.Move(velocity * Time.deltaTime); //캐릭터 이동
+
+        if (characterController.isGrounded) currentVelocityY = 0f;
     }
 
     public void Rotate()
     {
+        var targetRotation = followCam.transform.eulerAngles.y;
 
+        targetRotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, turnSmoothTime);
+
+        transform.eulerAngles = Vector3.up * targetRotation;
     }
 
     public void Jump()
     {
-        
+        if (!characterController.isGrounded) return; //공중에 있을 때 점프X
+        currentVelocityY = jumpVelocity;
     }
 
     private void UpdateAnimation(Vector2 moveInput)
     {
-
+        //현재 속도가 0이라면 걷는 애니메이션 작동X
+        var animationSpeedPercent = currentSpeed / speed;
+        animator.SetFloat("Vertical Move", moveInput.y* animationSpeedPercent,0.05f,Time.deltaTime);
+        animator.SetFloat("Horizontal Move", moveInput.x* animationSpeedPercent, 0.05f, Time.deltaTime);
     }
 }
