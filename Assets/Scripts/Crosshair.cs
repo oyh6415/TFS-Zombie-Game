14a4b7cccf278ecfb052d0bb359bdb6db74d82c0
oyh6415 <dudgus6324@naver.com>
2021-01-28 22:51:13 +0900

    UI 일부 구현
    * 탄창 수 표시
    * 조준점 구현

--- Assets/Scripts/Crosshair.cs
+++ Assets/Scripts/Crosshair.cs
@@ -3,33 +3,38 @@ using UnityEngine.UI;
 
 public class Crosshair : MonoBehaviour
 {
-    public Image aimPointReticle;
-    public Image hitPointReticle;
+    public Image aimPointReticle; //조준점
+    public Image hitPointReticle; //실제로 맞는 조준점
 
-    public float smoothTime = 10f;
+    public float smoothTime = 0.2f;
     
     private Camera screenCamera;
     private RectTransform crossHairRectTransform;
 
-    private Vector3 currentHitPointVelocity;
+    private Vector2 currentHitPointVelocity;
     private Vector2 targetPoint;
 
     private void Awake()
     {
-
+        screenCamera = Camera.main;
+        crossHairRectTransform = hitPointReticle.GetComponent<RectTransform>();
     }
 
     public void SetActiveCrosshair(bool active)
     {
-
+        hitPointReticle.enabled = active;
+        aimPointReticle.enabled = active;
     }
 
     public void UpdatePosition(Vector3 worldPoint)
     {
+        targetPoint = screenCamera.WorldToScreenPoint(worldPoint);
     }
 
     private void Update()
     {
-
+        if (!hitPointReticle.enabled) return; //조준점이 활성화되지 않았다면 종료
+        //실제 맞는 조준점 이동
+        crossHairRectTransform.position=Vector2.SmoothDamp(crossHairRectTransform.position,targetPoint,ref currentHitPointVelocity, smoothTime);
     }
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -6,7 +6,7 @@ public class UIManager : MonoBehaviour
 {
     private static UIManager instance;
     
-    public static UIManager Instance
+    public static UIManager Instance //싱글톤
     {
         get
         {
@@ -15,7 +15,7 @@ public class UIManager : MonoBehaviour
             return instance;
         }
     }
-
+    //인스펙터 창에서 설정할 수 있도록
     [SerializeField] private GameObject gameoverUI;
     [SerializeField] private Crosshair crosshair;
 
@@ -25,48 +25,48 @@ public class UIManager : MonoBehaviour
     [SerializeField] private Text ammoText;
     [SerializeField] private Text waveText;
 
-    public void UpdateAmmoText(int magAmmo, int remainAmmo)
+    public void UpdateAmmoText(int magAmmo, int remainAmmo) //탄약 표시
     {
         ammoText.text = magAmmo + "/" + remainAmmo;
     }
 
-    public void UpdateScoreText(int newScore)
+    public void UpdateScoreText(int newScore) //스코어 표시
     {
         scoreText.text = "Score : " + newScore;
     }
     
-    public void UpdateWaveText(int waves, int count)
+    public void UpdateWaveText(int waves, int count) //웨이브 표시
     {
         waveText.text = "Wave : " + waves + "\nEnemy Left : " + count;
     }
 
-    public void UpdateLifeText(int count)
+    public void UpdateLifeText(int count) //목숨 표시
     {
         lifeText.text = "Life : " + count;
     }
 
-    public void UpdateCrossHairPosition(Vector3 worldPosition)
+    public void UpdateCrossHairPosition(Vector3 worldPosition) //조준점 업데이트
     {
         crosshair.UpdatePosition(worldPosition);
     }
     
-    public void UpdateHealthText(float health)
+    public void UpdateHealthText(float health) //체력 업데이트
     {
         healthText.text = Mathf.Floor(health).ToString();
     }
     
-    public void SetActiveCrosshair(bool active)
+    public void SetActiveCrosshair(bool active) //조준점 표시
     {
         crosshair.SetActiveCrosshair(active);
     }
     
-    public void SetActiveGameoverUI(bool active)
+    public void SetActiveGameoverUI(bool active) //게임오버 UI 표시
     {
         gameoverUI.SetActive(active);
     }
     
-    public void GameRestart()
+    public void GameRestart() //Restart 버튼과 연결
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); //현재 씬을 재시작
     }
 }
